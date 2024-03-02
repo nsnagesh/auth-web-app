ce60e60036a25f854e9bb3aff750a73973b6c6ed
Nagesh Sanap <nageshsanap8181@gmail.com>
2024-03-03 01:54:32 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from 'pages/Home.jsx  ';
+import Home from './pages/Home';
 import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -14,11 +14,11 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Sign-in' element={<SignIn />} />
+        <Route path='/Sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/Profile' element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
